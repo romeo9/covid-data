8cd3ba535eb5c7468881b01e3f3eb3de95d3c216
romeo9 <cla.romeo9@gmail.com>
2020-03-23 12:18:59 +0100

    Updates

---
+++ precache-manifest.b1f0ce64effb47c9ed04c024fd689482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7dcb7fff9c1503ed0f2deafd04912b",
+    "url": "/covid-data/index.html"
+  },
+  {
+    "revision": "1fabc2a3db23488caa30",
+    "url": "/covid-data/static/css/main.10078280.chunk.css"
+  },
+  {
+    "revision": "0b8909c2135ea2fcc9ef",
+    "url": "/covid-data/static/js/2.f83bc103.chunk.js"
+  },
+  {
+    "revision": "760f42226fa49fe24abd3127835c05cd",
+    "url": "/covid-data/static/js/2.f83bc103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fabc2a3db23488caa30",
+    "url": "/covid-data/static/js/main.8b986e57.chunk.js"
+  },
+  {
+    "revision": "ea2a843100049ed206db",
+    "url": "/covid-data/static/js/runtime-main.81dc713b.js"
+  }
+]);
